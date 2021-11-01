a68c4991348dc029a05a666f11a9a404cd28ceb3
Leon Coto <leon.coto-rodriguez@weareadaptive.com>
2021-11-01 17:34:37 +0000

    caching and caching strategies

--- pwa-client/src/sw.js
+++ pwa-client/src/sw.js
@@ -1,27 +1,128 @@
 // Service Worker
-const pwaCache = "pwa-cache-1";
+const pwaCache = "pwa-cache-13";
+
+// Static assets to cache on install
+const staticCache = [
+  "/",
+  "style.css",
+  "main.js",
+  "index.html",
+  "thumb.png",
+  "warning.png",
+];
 
 // pretty much the same as
 // https://developer.mozilla.org/en-US/docs/Web/API/ExtendableEvent/waitUntil#example
 async function preCache() {
   const cache = await caches.open(pwaCache);
-  await cache.addAll(["/", "style.css", "thumb.png"]);
+  await cache.addAll(staticCache);
 }
 
-async function fetchCache(e) {
-  const cache = await caches.open(pwaCache);
+// strategies
 
-  const cachedResponse = await cache.match(e.request);
+// 1. Cache Only: Static Assets - App Shell
+async function cacheOnly(request) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/match
+  // assuming we don't store multiple versions of the same request
+  return caches.match(request);
+}
 
+// 2. Cache with Network Fallback
+async function cacheWithNetworkFallback(request) {
+  const cachedResponse = await caches.match(request);
   if (cachedResponse) {
-    console.log(`serving ${e.request.url} from cache`);
     return cachedResponse;
   } else {
-    console.log(`adding ${e.request.url} to cache`);
-    const fetchedResponse = await fetch(e.request);
-    cache.put(e.request, fetchedResponse.clone());
-    return fetchedResponse;
+    const networkResponse = await fetch(request);
+    const cache = await caches.open(pwaCache);
+    await cache.put(request, networkResponse.clone());
+    return networkResponse;
   }
+}
+
+// 3. Network with Cache Fallback
+async function networkWithCacheFallback(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      // update the cache anyway, in case in the future the network would fail
+      const cache = await caches.open(pwaCache);
+      await cache.put(request, networkResponse.clone());
+      return networkResponse;
+    } else {
+      return caches.match(request);
+    }
+  } catch {
+    return caches.match(request);
+  }
+}
+
+// 4. Cache with Network Update
+async function cacheWithNetworkUpdate(request) {
+  const cache = await caches.open(pwaCache);
+  const cachePromise = cache.match(request);
+
+  const networkPromise = fetch(request)
+    .then((networkResponse) => {
+      cache.put(request, networkResponse.clone());
+      return networkResponse;
+    })
+    .catch(() => new Response({ status: 503 }));
+
+  return (await cachePromise) ?? networkPromise;
+}
+
+// 5. Cache & Network Race
+async function cacheAndNetworkRace(request) {
+  const firstResponse = new Promise((resolve, reject) => {
+    let firstRejectionReceived = false;
+
+    const rejectOnce = () => {
+      if (firstRejectionReceived) {
+        reject("No response received");
+      } else {
+        firstRejectionReceived = true;
+      }
+    };
+
+    fetch(request)
+      .then(async (response) => {
+        if (response.ok) {
+          const cache = await caches.open(pwaCache);
+          cache.put(request, response.clone());
+          resolve(response);
+        } else {
+          rejectOnce();
+        }
+      })
+      .catch(rejectOnce);
+
+    caches
+      .match(request)
+      .then((response) => {
+        if (response) {
+          resolve(response);
+        } else {
+          rejectOnce();
+        }
+      })
+      .catch(rejectOnce);
+  });
+  return firstResponse;
+}
+
+async function cleanupCache() {
+  const keys = await caches.keys();
+
+  await Promise.all(
+    keys.map((key) => {
+      if (key !== pwaCache) {
+        return caches.delete(key);
+      } else {
+        return true;
+      }
+    })
+  );
 }
 
 self.addEventListener("install", async (e) => {
@@ -29,15 +130,13 @@ self.addEventListener("install", async (e) => {
   console.log("SW: Installed");
 });
 
-self.addEventListener("fetch", async (e) => {
-  // leave out all requests for external domains
-  if (e.request.url.match(location.origin)) {
-    e.respondWith(fetchCache(e));
-  }
+self.addEventListener("activate", (e) => {
+  e.waitUntil(cleanupCache());
+  console.log("SW: Activated");
 });
 
-self.addEventListener("activate", (e) => {
-  console.log("SW: Activated");
+self.addEventListener("fetch", async (e) => {
+  e.respondWith(cacheAndNetworkRace(e.request));
 });
 
 self.addEventListener("message", async (event) => {
